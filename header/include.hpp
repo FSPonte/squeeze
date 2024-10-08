8804f62229ea59ae1c4938d3ccccdc04fc463dcf
FSPonte <fsponte@outlook.com>
2024-10-09 00:11:59 +0100

    General purpose header file

---
+++ header/include.hpp
@@ -0,0 +1,16 @@
+#ifndef _INCLUDE_HPP_
+#define _INCLUDE_HPP_
+
+#include <vector>
+#include <fstream>
+
+typedef char byte_t;
+typedef unsigned long size_t;
+
+enum compress_type
+{
+    _none,
+    _rle
+};
+
+#endif // _INCLUDE_HPP_
