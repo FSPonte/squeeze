41636ba66a612b16aa4660143e4f4a044b086361
FSPonte <fsponte@outlook.com>
2024-10-11 18:29:56 +0100

    Changed nomenclature and added comments

--- header/include.hpp
+++ header/include.hpp
@@ -8,21 +8,24 @@
 typedef char byte_t;
 typedef unsigned long size_t;
 
-const size_t BUFFER_SIZE = 1024;
+const size_t BUFFER_SIZE = 1024; // Maximum size of a buffer
 
+// Process data type
 enum process_t
 {
     _invalid,
-    _compress,
-    _decompress
+    _encode,
+    _decode
 };
 
+// Algoritm data type
 enum algorithm_t
 {
     _none,
-    _rle
+    _rle        // Run-length enconding
 };
 
+// Buffer memory map data type
 union buffer_mmap
 {
     size_t size;
