18485f0b8285167d86549806af3cae3d2f29f97f
FSPonte <fsponte@outlook.com>
2024-10-16 23:08:12 +0100

    Added a function pointer data type and improved the enumerations

--- header/include.hpp
+++ header/include.hpp
@@ -1,12 +1,14 @@
 #ifndef _INCLUDE_HPP_
 #define _INCLUDE_HPP_
 
+#include <cstring>
 #include <cmath>
 #include <vector>
 #include <fstream>
 
 typedef char byte_t;
 typedef unsigned long size_t;
+typedef std::vector<byte_t> (*func_ptr)(const std::vector<byte_t>&) noexcept(true);
 
 const size_t BUFFER_SIZE = 1024; // Maximum size of a buffer
 const double PI = 3.14159265358979323846; // Pi constant
@@ -14,7 +16,7 @@ const double PI = 3.14159265358979323846; // Pi constant
 // Process data type
 enum process_t
 {
-    _invalid,
+    _invalid = -1,
     _encode,
     _decode
 };
@@ -22,8 +24,11 @@ enum process_t
 // Algoritm data type
 enum algorithm_t
 {
-    _none,
-    _rle        // Run-length enconding
+    _none = -1,
+    _rle = 0,       // Run-length enconding
+    _dct = 1,       // Discrete cosine transform
+
+    size            // Number of enumerators
 };
 
 // Buffer memory map data type
